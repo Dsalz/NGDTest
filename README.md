9e1043c0c8a249c9cd892f05d73df2239e292f85
Damola <salisu.damola@yahoo.com>
2019-01-30 05:13:49 +0100

    feat(loginPage): Implement login page UI and functionality

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# NGDTest
+# NGDTest App
+
+## Table of Contents
+
+* [About](#about)
+* [Getting Started](#gettingstarted)
+* [Dependencies](#dependencies)
+
+## About
+This is a simple app with just a login interface. Both email and password are required fields and the email address is validated before the login is successful. On successful login the locked image will change to an unlocked one. 
+
+
+## Getting Started
+
+- Clone repository to system
+- Open folder and run ```cd ngdtest``` to navigate to project
+- Run ```npm install``` to install dependencies
+- For development run ```npm start`` to start development server at port 3000
+- For production run ```npm run build``` to create production optimized project build
+- Configure hosted server to serve "/build/index.html"
+- To serve production build on local host run ```npm i -g serve && serve -s build``` to globally install a static server and serve the build at port 5000 (Don't forget to prefix this with ```sudo``` if you're using an iOS system)
+
+
+## Dependencies
+
+### Production dependencies
+- react
+- react-scripts
+- react-dom
+- Bootstrap (loaded via CDN)
+- Jquery for the Bootstrap JS file (loaded via CDN)
+
+### Development dependencies
+- eslint-config-airbnb
+- eslint-config-prettier
+- eslint-plugin-import
+- eslint-plugin-jsx-a11y
+- eslint-plugin-prettier
+- eslint-plugin-react
+- husky
+- lint-staged
+- prettier
+- pretty-quick
---
+++ ngdtest/src/components/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class App extends Component {
+  state = {
+    email: '',
+    password: '',
+    submitted: false,
+    errorMessage: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+      errorMessage: ''
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { email } = this.state;
+    const validEmail = /\w+[@]\w+[.]\w/.test(email);
+    if(validEmail){
+      this.setState({
+        submitted: true
+      })
+    }
+    else{
+      this.setState({
+        errorMessage: 'Invalid Email'
+      })
+    }
+  }
+
+  render() {
+      const { email , password, submitted, errorMessage } = this.state;
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <a className="navbar-brand" href="/">
+            <img className="navbar-brand-img" src="/images/logo.png" alt="NGD Logo" />
+            <span className="navbar-brand-text blue-cl" >scout</span>
+          </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"/>
+          </button>
+          <div className="collapse navbar-collapse nav-info" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="#nowhere">Product </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#nowhere">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#nowhere">Pricing</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link nav-btn round-edge blue-bg" href="#nowhere">Get Started</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <main className="signin-main">
+          <form className="signin-form" onSubmit={this.handleSubmit}>
+            <h2 className="blue-cl signin-form-header"> Sign in</h2>
+            <p className="signin-form-sub-header"> Sign into our platform below</p>
+            <span className="signin-form-error">{errorMessage}</span>
+            <input name='email' placeholder="Email address" value={email} onChange={this.handleChange} type="text" className="round-edge signin-form-input" required/>
+            <input name='password' placeholder="Password" value={password} onChange={this.handleChange} type="password" className="round-edge signin-form-input" required/>
+            <button type="submit" className="round-edge signin-form-submit-btn blue-bg">
+              <img src={submitted ? '/images/unlock.png': '/images/lock.png'} alt= {submitted ? 'unlocked': 'locked'} className="signin-form-submit-btn-img"/>
+            </button>
+            <a className="blue-cl signin-form-link" href="#nowhere">Forgot your password?</a>
+          </form>
+          <div className="signin-xtra-links">
+          <a className="blue-cl signin-xtra-link" href="#nowhere">Terms of Service</a>
+          <a className="blue-cl signin-xtra-link" href="#nowhere">Privacy Policy</a>            
+          </div>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
