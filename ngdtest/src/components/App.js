72de7af06a44b754429f5e910f2c0769c4952215
Damola <salisu.damola@yahoo.com>
2019-01-30 05:24:17 +0100

    chore(comments): Add Comments to component files

--- ngdtest/src/components/App.js
+++ ngdtest/src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+// CSS
 import '../css/App.css';
 
 class App extends Component {
@@ -17,8 +19,11 @@ class App extends Component {
   }
 
   handleSubmit = (e) => {
+    // Prevent Page Reloading
     e.preventDefault();
     const { email } = this.state;
+
+    // Validate email before accepting submission
     const validEmail = /\w+[@]\w+[.]\w/.test(email);
     if(validEmail){
       this.setState({
@@ -36,47 +41,61 @@ class App extends Component {
       const { email , password, submitted, errorMessage } = this.state;
     return (
       <React.Fragment>
+        {/* Navbar */}
         <nav className="navbar navbar-expand-lg navbar-light">
-          <a className="navbar-brand" href="/">
-            <img className="navbar-brand-img" src="/images/logo.png" alt="NGD Logo" />
-            <span className="navbar-brand-text blue-cl" >scout</span>
-          </a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"/>
-          </button>
-          <div className="collapse navbar-collapse nav-info" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#nowhere">Product </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#nowhere">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#nowhere">Pricing</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link nav-btn round-edge blue-bg" href="#nowhere">Get Started</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <main className="signin-main">
-          <form className="signin-form" onSubmit={this.handleSubmit}>
-            <h2 className="blue-cl signin-form-header"> Sign in</h2>
-            <p className="signin-form-sub-header"> Sign into our platform below</p>
-            <span className="signin-form-error">{errorMessage}</span>
-            <input name='email' placeholder="Email address" value={email} onChange={this.handleChange} type="text" className="round-edge signin-form-input" required/>
-            <input name='password' placeholder="Password" value={password} onChange={this.handleChange} type="password" className="round-edge signin-form-input" required/>
-            <button type="submit" className="round-edge signin-form-submit-btn blue-bg">
-              <img src={submitted ? '/images/unlock.png': '/images/lock.png'} alt= {submitted ? 'unlocked': 'locked'} className="signin-form-submit-btn-img"/>
+            
+            {/* Logo */}
+            <a className="navbar-brand" href="/">
+                <img className="navbar-brand-img" src="/images/logo.png" alt="NGD Logo" />
+                <span className="navbar-brand-text blue-cl" >scout</span>
+            </a>
+          
+            {/* Responsive Toggle Button */}
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"/>
             </button>
-            <a className="blue-cl signin-form-link" href="#nowhere">Forgot your password?</a>
-          </form>
-          <div className="signin-xtra-links">
-          <a className="blue-cl signin-xtra-link" href="#nowhere">Terms of Service</a>
-          <a className="blue-cl signin-xtra-link" href="#nowhere">Privacy Policy</a>            
-          </div>
+          
+            {/* Navbar Links */}
+            <div className="collapse navbar-collapse nav-info" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                       <a className="nav-link" href="#nowhere">Product </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#nowhere">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#nowhere">Pricing</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link nav-btn round-edge blue-bg" href="#nowhere">Get Started</a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+
+        {/* Main Section */}
+        <main className="signin-main">
+
+            {/* Sign In Form */}
+            <form className="signin-form" onSubmit={this.handleSubmit}>
+                <h2 className="blue-cl signin-form-header"> Sign in</h2>
+                <p className="signin-form-sub-header"> Sign into our platform below</p>
+                <span className="signin-form-error">{errorMessage}</span>
+                <input name='email' placeholder="Email address" value={email} onChange={this.handleChange} type="text" className="round-edge signin-form-input" required/>
+                <input name='password' placeholder="Password" value={password} onChange={this.handleChange} type="password" className="round-edge signin-form-input" required/>
+                <button type="submit" className="round-edge signin-form-submit-btn blue-bg">
+                  <img src={submitted ? '/images/unlock.png': '/images/lock.png'} alt= {submitted ? 'unlocked': 'locked'} className="signin-form-submit-btn-img"/>
+                </button>
+                <a className="blue-cl signin-form-link" href="#nowhere">Forgot your password?</a>
+            </form>
+
+            {/* Form Footer Links */}
+            <div className="signin-xtra-links">
+                <a className="blue-cl signin-xtra-link" href="#nowhere">Terms of Service</a>
+                <a className="blue-cl signin-xtra-link" href="#nowhere">Privacy Policy</a>            
+            </div>
         </main>
       </React.Fragment>
     );
--- ngdtest/src/index.js
+++ ngdtest/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// Components
 import App from './components/App';
+
+// CSS
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
